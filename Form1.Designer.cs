83ec51ac3bdfee5c28c045319e87f4b349ef4dc0
Harkiran Grewal <40836336+harkirangrewal@users.noreply.github.com>
2019-08-20 15:01:59 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Fractals_Sierpinski
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.txtLevel = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Location = new System.Drawing.Point(652, 53);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(46, 20);
+            this.lblLevel.TabIndex = 0;
+            this.lblLevel.Text = "Level";
+            // 
+            // txtLevel
+            // 
+            this.txtLevel.Location = new System.Drawing.Point(729, 53);
+            this.txtLevel.Name = "txtLevel";
+            this.txtLevel.Size = new System.Drawing.Size(100, 26);
+            this.txtLevel.TabIndex = 1;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(656, 147);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(173, 50);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(605, 605);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 645);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtLevel);
+            this.Controls.Add(this.lblLevel);
+            this.Name = "Form1";
+            this.Text = "Sierpinski Triangle";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.TextBox txtLevel;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractals_Sierpinski
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Generate_Sierpinski(int n, Graphics g, Point p1, Point p2, Point p3)
+        {
+            if (n == 0)
+                return;
+            else
+            {
+                Point[] triangle = new Point[3];
+                Point m1, m2, m3;
+                m1 = new Point();
+                m2 = new Point();
+                m3 = new Point();
+
+                triangle[0] = p1;
+                triangle[1] = p2;
+                triangle[2] = p3;
+
+                Pen myPen = new Pen(Color.Red, 1);
+                g.DrawPolygon(myPen, triangle);
+
+                // midpoint of linesegment p1p2
+                m1.X = (p1.X + p2.X) / 2;
+                m1.Y = (p1.Y + p2.Y) / 2;
+
+                // midpoint of linesegment p1p3
+                m2.X = (p1.X + p3.X) / 2;
+                m2.Y = (p1.Y + p3.Y) / 2;
+
+                // midpoint of linesegment p2p3
+                m3.X = (p2.X + p3.X) / 2;
+                m3.Y = (p2.Y + p3.Y) / 2;
+
+                Generate_Sierpinski(n - 1, g, p1, m1, m2);
+                Generate_Sierpinski(n - 1, g, m1, p2, m3);
+                Generate_Sierpinski(n - 1, g, m2, m3, p3);
+            }
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            int n; Point p1, p2, p3;
+            n = Convert.ToInt32(txtLevel.Text);
+
+            p1 = new Point();
+            p2 = new Point(); p3 = new Point();
+
+            p1.X = 300; p1.Y = 1; p2.X = 1; p2.Y = 600;
+            p3.X = 600; p3.Y = 600;
+
+            Graphics g = Graphics.FromHwnd(pictureBox1.Handle); g.Clear(Color.White);
+            Generate_Sierpinski(n, g, p1, p2, p3);
+        }
+    }
+}
